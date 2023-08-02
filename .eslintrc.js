0b3108061f1f3265f69b96236b87d30cdea16914
brygmos <daniel-best@yandex.ru>
2023-08-03 01:11:03 +0300

    add store, entity, tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,8 @@ module.exports = {
         'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
         'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
         'jsx-a11y/no-static-element-interactions': 'off',
+        'arrow-body-style': 'off',
+        'no-param-reassign': 'off',
     },
     globals: {
         __IS_DEV__: true,
