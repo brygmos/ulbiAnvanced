f38ac06cc9136996fd92d2fc09995a1ecb6e3e10
brygmos <daniel-best@yandex.ru>
2023-08-03 17:35:53 +0300

    add input storybook, LoginForm storybook

--- src/shared/ui/Input/ui/Input.stories.tsx
+++ src/shared/ui/Input/ui/Input.stories.tsx
@@ -1,104 +1,19 @@
-// import React from 'react';
-// import { ComponentStory, ComponentMeta } from '@storybook/react';
-//
-// import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-// import { Theme } from 'app/providers/ThemeProvider';
-// import { Input, ButtonSize, ThemeButton } from './Input';
-//
-// export default {
-//     title: 'shared/Input',
-//     component: Input,
-//     argTypes: {
-//         backgroundColor: { control: 'color' },
-//     },
-// } as ComponentMeta<typeof Input>;
-//
-// const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
-//
-// export const Primary = Template.bind({});
-// Primary.args = {
-//     children: 'Text',
-// };
-//
-// export const Clear = Template.bind({});
-// Clear.args = {
-//     children: 'Text',
-//     theme: ThemeButton.CLEAR,
-// };
-//
-// export const ClearInverted = Template.bind({});
-// ClearInverted.args = {
-//     children: 'Text',
-//     theme: ThemeButton.CLEAR_INVERTED,
-// };
-//
-// export const Outline = Template.bind({});
-// Outline.args = {
-//     children: 'Text',
-//     theme: ThemeButton.OUTLINE,
-// };
-//
-// export const OutlineSizeL = Template.bind({});
-// OutlineSizeL.args = {
-//     children: 'Text',
-//     theme: ThemeButton.OUTLINE,
-//     size: ButtonSize.L,
-// };
-//
-// export const OutlineSizeXL = Template.bind({});
-// OutlineSizeXL.args = {
-//     children: 'Text',
-//     theme: ThemeButton.OUTLINE,
-//     size: ButtonSize.XL,
-// };
-//
-// export const OutlineDark = Template.bind({});
-// OutlineDark.args = {
-//     children: 'Text',
-//     theme: ThemeButton.OUTLINE,
-// };
-//
-// export const BackgroundTheme = Template.bind({});
-// BackgroundTheme.args = {
-//     children: 'Text',
-//     theme: ThemeButton.BACKGROUND,
-// };
-//
-// export const BackgroundInverted = Template.bind({});
-// BackgroundInverted.args = {
-//     children: 'Text',
-//     theme: ThemeButton.BACKGROUND_INVERTED,
-// };
-//
-// export const Square = Template.bind({});
-// Square.args = {
-//     children: '>',
-//     theme: ThemeButton.BACKGROUND_INVERTED,
-//     square: true,
-// };
-//
-// export const SquareSizeM = Template.bind({});
-// SquareSizeM.args = {
-//     children: '>',
-//     theme: ThemeButton.BACKGROUND_INVERTED,
-//     square: true,
-//     size: ButtonSize.M,
-// };
-//
-// export const SquareSizeL = Template.bind({});
-// SquareSizeL.args = {
-//     children: '>',
-//     theme: ThemeButton.BACKGROUND_INVERTED,
-//     square: true,
-//     size: ButtonSize.L,
-// };
-//
-// export const SquareSizeXL = Template.bind({});
-// SquareSizeXL.args = {
-//     children: '>',
-//     theme: ThemeButton.BACKGROUND_INVERTED,
-//     square: true,
-//     size: ButtonSize.XL,
-// };
-//
-// OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Input } from 'shared/ui/Input';
+
+export default {
+    title: 'shared/Input',
+    component: Input,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    placeholder: 'Type text',
+    value: '123123',
+};
