e9f2802ec12b7a44438a6e05a7ddaaf6bcab346a
brygmos <daniel-best@yandex.ru>
2023-08-03 13:17:26 +0300

    add json-server

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -54,12 +54,11 @@ export const Modal = ({
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        // [cls[theme]]: true,
     };
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div
                         className={cls.content}
