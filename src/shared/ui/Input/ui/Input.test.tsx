f26ae41213abdc92668b9ec12af277d52e14d170
brygmos <daniel-best@yandex.ru>
2023-08-05 00:37:29 +0300

    add login tests,

--- src/shared/ui/Input/ui/Input.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import { Input } from './Input';
-//
-// describe('classNames', () => {
-//     test('test', () => {
-//         render(<Input>TEST</Input>);
-//         expect(screen.getByText('TEST')).toBeInTheDocument();
-//     });
-// });
--- src/shared/ui/Text/ui/Text.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import { Button } from './Button';
-//
-// describe('classNames', () => {
-//     test('test', () => {
-//         render(<Button>TEST</Button>);
-//         expect(screen.getByText('TEST')).toBeInTheDocument();
-//     });
-// });
