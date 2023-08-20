ea603c1aee7653c4f17a2aea120450a42e0fdf70
brygmos <daniel-best@yandex.ru>
2023-08-21 01:19:06 +0300

    add: custom eslint plugin

--- src/widgets/PageLoader/ui/PageLoader.tsx
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { Loader } from 'shared/ui/Loader';
+import { classNames } from 'src/shared/lib/classNames/classNames';
+import { Loader } from 'src/shared/ui/Loader';
 import cls from './PageLoader.module.scss';
 
 type PageLoaderProps = {
