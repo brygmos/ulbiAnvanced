8b598cf7eaaad993dfa5d529d1f6fec6d71991d1
brygmos <daniel-best@yandex.ru>
2023-08-25 01:29:01 +0300

    add: AnimationProvider, dynamic animation libs loading

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -8,6 +8,7 @@ import { Popover } from 'shared/ui/Popups';
 import { BrowserView, MobileView } from 'react-device-detect';
 import { Drawer } from 'shared/ui/Drawer/Drawer';
 import cls from './NotificationButton.module.scss';
+import { AnimationProvider } from '../../../../shared/lib/components/AnimationProvider';
 
 interface NotificationButtonProps {
     className?: string;
@@ -45,9 +46,11 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
             </BrowserView>
             <MobileView>
                 {trigger}
-                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                    <NotificationList />
-                </Drawer>
+                <AnimationProvider>
+                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                        <NotificationList />
+                    </Drawer>
+                </AnimationProvider>
             </MobileView>
         </div>
 
