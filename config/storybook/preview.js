928c14d898ec2bfe4b51eb3257c771dcf91932c8
brygmos <daniel-best@yandex.ru>
2023-08-27 00:53:49 +0300

    add: storybook theme addon

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,7 +1,7 @@
 import { addDecorator } from '@storybook/react';
 import { StyleDecorator } from 'shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/providers/ThemeProvider';
+import { Theme } from '@/shared/const/theme';
 import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
 import {SuspenseDecorator} from 'shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
 
@@ -12,6 +12,15 @@ export const parameters = {
             color: /(background|color)$/i,
             date: /Date$/,
         },
+    },
+    layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+            { name: 'dark', class: Theme.DARK, color: '#000000' },
+            { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+        ],
     },
 };
 
