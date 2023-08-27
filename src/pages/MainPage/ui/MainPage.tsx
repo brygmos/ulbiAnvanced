feb29e9937fc7faf4164e2bf50d2cf97e5c00ddc
brygmos <daniel-best@yandex.ru>
2023-08-27 20:23:45 +0300

    add: helpers for slice, selectors, actions

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { Counter } from '@/entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
         <Page>
+            <Counter />
             <p>{t('Главная страница')}</p>
         </Page>
     );
