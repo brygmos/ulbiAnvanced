d5ae1c5c086b0d55781ab020f260cac305d2551d
brygmos <daniel-best@yandex.ru>
2023-08-30 13:43:24 +0300

    add: e2e stubs

--- cypress/e2e/article/articles-list.cy.ts
+++ cypress/e2e/article/articles-list.cy.ts
@@ -5,6 +5,7 @@ describe('User enters articles list page', () => {
         });
     });
     it('articles loads successfully', () => {
+        cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
         cy.getByTestId('ArticleList').should('exist');
         cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
     });
